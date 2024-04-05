5813bf89ec75e9f28d14e71501e41b4979225ce6
ctorres <ctorres.developer@gmail.com>
2024-04-05 14:52:59 -0600

    Fix unused import and unused label in the Manifest

--- Composables/src/debug/kotlin/com/chefsito/rickmorty/composables/UImagePreview.kt
+++ Composables/src/debug/kotlin/com/chefsito/rickmorty/composables/UImagePreview.kt
@@ -2,7 +2,6 @@ package com.chefsito.rickmorty.composables
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import coil.compose.AsyncImage
 
 @Preview
 @Composable

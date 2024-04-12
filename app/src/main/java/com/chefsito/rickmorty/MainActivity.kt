24042aeb76f1112c49f22f92dcda3169b63369ff
ctorres <ctorres.developer@gmail.com>
2024-04-11 23:10:19 -0600

    Show the GraphQL info in the VIEW

--- app/src/main/java/com/chefsito/rickmorty/MainActivity.kt
+++ app/src/main/java/com/chefsito/rickmorty/MainActivity.kt
@@ -3,18 +3,26 @@ package com.chefsito.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chefsito.rickmorty.composables.UCharacterCard
 import com.chefsito.rickmorty.ui.theme.RickMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel: MainActivityViewModel by viewModels()
+        viewModel.getCharacterList()
         setContent {
             RickMortyTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +30,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    UICh
+                    val data = viewModel.charactersState.collectAsState()
+                    LazyColumn(modifier = Modifier.padding(vertical = 5.dp)) {
+                        items(data.value) {
+                            UCharacterCard(
+                                modifier = Modifier,
+                                name = it.name,
+                                gender = it.genter,
+                                url = it.urlImage,
+                                created = it.created
+                            )
+                        }
+                    }
                 }
             }
         }

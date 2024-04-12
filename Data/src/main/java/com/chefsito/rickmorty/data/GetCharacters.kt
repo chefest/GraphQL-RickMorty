ea5182004b4b75b13710542cc9d8c0984280cf51
ctorres <ctorres.developer@gmail.com>
2024-04-11 21:01:21 -0600

    Fix: Rename classname and the return type for the function.

--- Data/src/main/java/com/chefsito/rickmorty/data/GetCharacters.kt
+++ Data/src/main/java/com/chefsito/rickmorty/data/GetCharacters.kt
@@ -7,14 +7,15 @@ import com.chefsito.rickmorty.data.graphql.GetCharactersQuery
  * This class use MyApolloClient to handle the communication
  * between the query and the server
  */
-class GetCharactersQuery(
+class GetCharacters(
     val myApolloClient: MyApolloClient
 ) {
     /**
      * This function use the Apollo client
      * and get the character list
      */
-    suspend fun GetCharacterList(){
+    suspend fun GetCharacterList(): GetCharactersQuery.Data? {
         val response = myApolloClient.get().query(GetCharactersQuery()).execute()
+        return response.data
     }
 }

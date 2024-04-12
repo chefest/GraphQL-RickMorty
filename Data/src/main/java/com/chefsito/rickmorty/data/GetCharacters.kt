4ed087f6796ec9dd80d8e8837d4ad97ee2480044
ctorres <ctorres.developer@gmail.com>
2024-04-11 23:07:09 -0600

    Feat: Use a Data Model to return the GraphQL data to the VIEW Layer

--- Data/src/main/java/com/chefsito/rickmorty/data/GetCharacters.kt
+++ Data/src/main/java/com/chefsito/rickmorty/data/GetCharacters.kt
@@ -2,20 +2,40 @@ package com.chefsito.rickmorty.data
 
 import com.chefsito.rickmorty.MyApolloClient
 import com.chefsito.rickmorty.data.graphql.GetCharactersQuery
+import com.chefsito.rickmorty.models.CharacterDataModel
 
 /**
  * This class use MyApolloClient to handle the communication
  * between the query and the server
  */
-class GetCharacters(
-    val myApolloClient: MyApolloClient
+class GetCharacters constructor (
+    private val myApolloClient: MyApolloClient
 ) {
     /**
      * This function use the Apollo client
      * and get the character list
      */
-    suspend fun GetCharacterList(): GetCharactersQuery.Data? {
-        val response = myApolloClient.get().query(GetCharactersQuery()).execute()
-        return response.data
+    suspend fun GetCharacterList(): List<CharacterDataModel> {
+        var listOfCharacters = listOf<CharacterDataModel>()
+        try {
+            val characters = myApolloClient
+                .get()
+                .query(GetCharactersQuery())
+                .execute()
+                .data?.characters
+            characters?.results?.let {
+                listOfCharacters = it.map {
+                    CharacterDataModel(
+                        name = it?.name?:"",
+                        genter = it?.gender?:"",
+                        created = it?.created?:"",
+                        urlImage = it?.image?:""
+                    )
+                }
+            }
+        } catch (ex: Exception) {
+            ex.toString()
+        }
+        return listOfCharacters
     }
 }
---
+++ Data/src/main/java/com/chefsito/rickmorty/models/CharacterDataModel.kt
@@ -0,0 +1,8 @@
+package com.chefsito.rickmorty.models
+
+data class CharacterDataModel (
+    val name: String,
+    val genter: String,
+    val created: String,
+    val urlImage: String
+)

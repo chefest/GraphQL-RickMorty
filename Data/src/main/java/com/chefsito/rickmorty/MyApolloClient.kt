9e1e1e2b53cb2c2f17c1f3f32b3abd78d3d85eb1
ctorres <ctorres.developer@gmail.com>
2024-04-11 22:29:58 -0600

    MyApolloClient deprecated code removed

--- Data/src/main/java/com/chefsito/rickmorty/MyApolloClient.kt
+++ Data/src/main/java/com/chefsito/rickmorty/MyApolloClient.kt
@@ -1,25 +1,19 @@
 package com.chefsito.rickmorty
 
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import com.chefsito.rickmorty.data.BuildConfig
 
 class MyApolloClient {
 
-    @Deprecated("Singleton is not needed anymore")
-    companion object {
-        private lateinit var INSTANCE: MyApolloClient
-        fun getInstance(): MyApolloClient {
-            if (INSTANCE == null) {
-                INSTANCE = MyApolloClient()
-            }
-            return INSTANCE
-        }
+    var apolloClient: ApolloClient
+    init {
+        Log.d(this.javaClass.name,"Instance: MyApolloClient created" )
+        apolloClient = ApolloClient
+            .Builder()
+            .serverUrl(BuildConfig.APOLLO_SERVER)
+            .build()
     }
-
-    private var apolloClient: ApolloClient = ApolloClient
-        .Builder()
-        .serverUrl(BuildConfig.APOLLO_SERVER)
-        .build()
 
     fun get(): ApolloClient {
         return apolloClient
